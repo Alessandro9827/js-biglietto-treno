00b9cee7e8c328956208f32d0613e6aa52de2991
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-11 20:30:02 +0100

    input utente

---
+++ script.js
@@ -0,0 +1,3 @@
+/*Chiediamo all'utente i km che vuole percorrere e l'età*/
+const kmPercorsi = parseFloat (prompt("Inserisci qui i KM previsti da percorrere"))
+const etaUtente = parseInt (prompt("Inserisci l'età del viaggiatore"))
